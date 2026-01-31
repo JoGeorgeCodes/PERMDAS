308685c75cc1c9acc198a951727de057f89c1393
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-01-30 20:24:42 -0800

    Add functionality to update server stats

--- updateserverstats.js
+++ updateserverstats.js
@@ -1,1 +1,35 @@
+const stats = {
+	username: localStorage.username,
+	passwordHash: localStorage.passwordHash,
 
+	bestScore: localStorage.bestScore,
+	bestTime: localStorage.bestTime,
+
+	totalGames: localStorage.totalGames,
+	totalEquations: localStorage.totalEquations,
+
+	soundEnabled: localStorage.soundEnabled,
+	musicEnabled: localStorage.musicEnabled,
+	correctSound: localStorage.correctSound,
+	wrongSound: localStorage.wrongSound,
+
+	answer_question: localStorage.answer_question,
+	reset_game: localStorage.reset_game,
+
+	unlockedAchievements: localStorage.unlockedAchievements
+};
+
+fetch(API + "/setStats", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(stats)
+})
+.then(res => res.json())
+.then(data => {
+  if(data.success == true){
+    alert("Sucess")
+    localStorage.passwordHash = newPasswordHash
+  }else{
+    alert("Something Went Wrong")
+  }
+})
