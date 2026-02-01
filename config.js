5f58cd71f0ff22f8c73eacea259f51ebdd2fe8f8
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-01-31 17:08:04 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-var API = "https://jewellery-upon-beautifully-owen.trycloudflare.com/";
+var API = "https://jewellery-upon-beautifully-owen.trycloudflare.com/";//URGENT MAKE SURE TO REMOVE SLASH AT END
+
+//of course the programmer forgot
+if (API.endsWith('/')) {
+  // slice(0, -1) creates a new string from the start up to the second-to-last character
+  API = str.slice(0, -1);
+}
