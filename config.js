1bd005dde1c995527e97ab2188b5265f19f2383f
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-02-09 17:01:52 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var API = "https://thompson-implies-tongue-annotation.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
+var API = "https://dozen-rugs-dts-performing.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
 
 //of course the programmer forgot
 if (API.endsWith('/')) {
