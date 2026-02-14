fdf728a0114cf2a8edf92df75021d8f0a91d9e58
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-02-13 16:05:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -95,11 +95,12 @@ app.post("/enter-matchmaking", (req, res) => {
 		return res.status(401).json({
 			error: "not authorized (password wrong)"
 		});
-	if(peopleInMatchmaking.some((e)=>(e.name == name))){
-		return res.status(409).json({
-			error: "i hope your not a clone, bc you can only sign up ONCE --when kids code"
-		});
-	}
+	peopleInMatchmaking = peopleInMatchmaking.filter((e) => (e.name != name));
+	// if(peopleInMatchmaking.some((e)=>(e.name == name))){
+	// 	return res.status(409).json({
+	// 		error: "i hope your not a clone, bc you can only sign up ONCE --when kids code"
+	// 	});
+	// }
 	if(peopleInMatchmaking.length > 0){
 		//create a room for both
 		var room = createDuel();
