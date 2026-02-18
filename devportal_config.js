e72a54e75653ec3ec2d96176fb39ee6b4a09e025
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-02-18 11:31:22 -0800

    Update devportal_config.js

--- devportal_config.js
+++ devportal_config.js
@@ -1,1 +1,1 @@
-var devportal_API = "https://warranties-limousines-keep-lace.trycloudflare.com" //urgent: remove slash at the end
+var devportal_API = "https://educational-beans-suppose-instrumental.trycloudflare.com" //urgent: remove slash at the end
