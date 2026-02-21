c6cce1096132abc6412cfb76231071d2a57e5e63
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-02-20 16:00:53 -0800

    Update devportal_config.js

--- devportal_config.js
+++ devportal_config.js
@@ -1,1 +1,1 @@
-var devportal_API = "https://educational-beans-suppose-instrumental.trycloudflare.com" //urgent: remove slash at the end
+var devportal_API = "https://additional-comparison-accepting-administrator.trycloudflare.com" //urgent: remove slash at the end
