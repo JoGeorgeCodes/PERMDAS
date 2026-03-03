fd18a5065419b905c72609d8bbf5a0db4a7c9341
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-03-02 19:17:54 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var API = "https://vast-dylan-downloading-vote.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
+var API = "https://increasing-short-recipients-weather.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
 
 //of course the programmer forgot
 if (API.endsWith('/')) {
