19389e3ce9a8be8fb327567677239567a3aea794
Johann George <johanntgeorge@gmail.com>
2026-03-06 13:14:22 -0800

    changed api to render url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-var API = "https://inspired-affordable-ranges-tourist.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
+var API = "https://permdas-x8zj.onrender.com/";//URGENT MAKE SURE TO REMOVE SLASH AT END
 
 //of course the programmer forgot
 if (API.endsWith('/')) {
   // slice(0, -1) creates a new string from the start up to the second-to-last character
-  API = str.slice(0, -1);
+  API = API.slice(0, -1);
 }
