6ba8adb27d25acbe7d25197a612d67621ac6c8c0
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-03-11 16:22:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var API = "https://inspired-affordable-ranges-tourist.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
+var API = "https://ben-mothers-inspection-broke.trycloudflare.com";//URGENT MAKE SURE TO REMOVE SLASH AT END
 
 //of course the programmer forgot
 if (API.endsWith('/')) {
